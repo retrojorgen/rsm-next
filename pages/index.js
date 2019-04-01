ce81eb608fd84d4ba30aaa56d04dafe99a7d3aa6
Jørgen Jacobsen <jorgeja@gmail.com>
2019-04-01 13:23:09 +0200

    kmlode

--- pages/index.js
+++ pages/index.js
@@ -387,6 +387,7 @@ export default () => (
             </a>
           </p>
           <h4>Overnatting:</h4>
+          <u>Bestillingskode nett: BRSM210619</u><br><br>
           <p>
             Planlegger du å bli hele helgen på Retrospillmessen? Da bør du bo på
             Sandefjords største hotell{" "}
@@ -407,7 +408,7 @@ export default () => (
             .<br />
             <br />
             <strong>
-              <u>Bestillingskode nett: BRSM210619</u>
+              <u>Bestillingskode nett: BRSM210619</u><br><br>
             </strong>
             <strong>Priser (inkludert frokost):</strong>
             <br />
