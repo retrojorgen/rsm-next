67bce569419078a31569b6ff4e2b8fbc96388c18
Jørgen Jacobsen <jorgeja@gmail.com>
2019-04-04 21:16:09 +0200

    seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -6,9 +6,27 @@
  */
 
 import React from "react"
+import PropTypes from "prop-types"
 import Helmet from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
-export default ({ description, lang, meta, keywords, title }) => {
+function SEO({ description, lang, meta, keywords, title }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+
   return (
     <Helmet
       htmlAttributes={{
@@ -62,3 +80,20 @@ export default ({ description, lang, meta, keywords, title }) => {
     />
   )
 }
+
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  keywords: [],
+  description: ``,
+}
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  keywords: PropTypes.arrayOf(PropTypes.string),
+  title: PropTypes.string.isRequired,
+}
+
+export default SEO
